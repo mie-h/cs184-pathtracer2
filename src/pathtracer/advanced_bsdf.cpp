dc5ef04937935832a03ca5f8ce593e176e62248b
mie-h <56993977+mie-h@users.noreply.github.com>
2021-04-04 21:01:39 -0700

    final commit

--- src/pathtracer/advanced_bsdf.cpp
+++ src/pathtracer/advanced_bsdf.cpp
@@ -22,6 +22,9 @@ Vector3D MirrorBSDF::sample_f(const Vector3D wo, Vector3D* wi, double* pdf) {
 
   // TODO Project 3-2: Part 1
   // Implement MirrorBSDF
+    *pdf = 1.0;
+    reflect(wo, wi);
+    return reflectance / abs_cos_theta(*wi);
   return Vector3D();
 }
 
@@ -92,6 +95,25 @@ Vector3D RefractionBSDF::f(const Vector3D wo, const Vector3D wi) {
 Vector3D RefractionBSDF::sample_f(const Vector3D wo, Vector3D* wi, double* pdf) {
   // TODO Project 3-2: Part 1
   // Implement RefractionBSDF
+   
+  // task 3
+    *pdf = 1;
+    if (!refract(wo, wi, ior)) {
+        return Vector3D();
+    }
+    float eta;
+    if (wo.z > 0) {
+        eta = ior;
+    }
+    else {
+        eta = 1.0 / ior;
+    }
+    return transmittance / abs_cos_theta(*wi) / ( eta * eta);
+
+
+
+
+
   return Vector3D();
 }
 
@@ -118,6 +140,44 @@ Vector3D GlassBSDF::sample_f(const Vector3D wo, Vector3D* wi, double* pdf) {
 
   // compute Fresnel coefficient and use it as the probability of reflection
   // - Fundamentals of Computer Graphics page 305
+
+    // total internal reflection
+  if (!refract(wo, wi, ior)) {
+      // assign reflection of wo to wi
+      reflect(wo, wi);
+      // set pdf to 1
+      *pdf = 1;
+      return reflectance / abs_cos_theta(*wi);
+  }
+  else {
+      // compute schlick's R
+      float n1 = 1.0;
+      float n2 = ior;
+      float R0 = pow(((n1 - n2) / (n1 + n2)), 2);
+      float schlick_r = R0 + (1 - R0) * pow((1 - abs_cos_theta(wo)), 5);
+      if (coin_flip(schlick_r)) {
+          // assign ref wo to wi
+          reflect(wo, wi);
+          *pdf = 1;
+          return schlick_r * reflectance / abs_cos_theta(*wi);
+     }
+      else {
+          refract(wo, wi, ior);
+          *pdf = 1 - schlick_r;
+
+          float eta;
+          if (wo.z > 0) {
+              eta = ior;
+          }
+          else {
+              eta = 1.0 / ior;
+          }
+          return (1.0 - schlick_r) * transmittance / abs_cos_theta(*wi) / (eta * eta);
+
+          
+      }
+  }
+  
   return Vector3D();
 }
 
@@ -136,7 +196,7 @@ void BSDF::reflect(const Vector3D wo, Vector3D* wi) {
 
   // TODO Project 3-2: Part 1
   // Implement reflection of wo about normal (0,0,1) and store result in wi.
-
+    *wi = Vector3D(-wo[0], -wo[1], wo[2]);
 
 }
 
@@ -147,6 +207,30 @@ bool BSDF::refract(const Vector3D wo, Vector3D* wi, double ior) {
   // Return false if refraction does not occur due to total internal reflection
   // and true otherwise. When dot(wo,n) is positive, then wo corresponds to a
   // ray entering the surface through vacuum.
+    float n;
+    if (wo.z > 0) {
+        n = 1 / ior;
+    }
+    else {
+        n = ior;
+    }
+
+    float inner = 1 - pow(n, 2) * (1 - pow(wo.z, 2));
+    if (inner < 0) {
+        return false;
+    }
+    float new_x = -1 * n * wo.x;
+    float new_y = -1 * n * wo.y;
+    float new_z;
+    if (wo.z > 0) {
+        new_z = sqrt(inner) * -1;
+    }
+    else {
+        new_z = sqrt(inner);
+    }
+    *wi = Vector3D(new_x, new_y, new_z);
+
+
 
   return true;
 
--- src/pathtracer/camera_lens.cpp
+++ src/pathtracer/camera_lens.cpp
@@ -24,8 +24,36 @@ Ray Camera::generate_ray_for_thin_lens(double x, double y, double rndR, double r
   // TODO Project 3-2: Part 4
   // compute position and direction of ray from the input sensor sample coordinate.
   // Note: use rndR and rndTheta to uniformly sample a unit disk.
-  
-  return Ray(pos, Vector3D(0, 0, -1));
+
+
+    //Look up your code from Project 3-1, Part 1 to figure out the generated ray direction (red segment in the figure).
+    double radian_conv = 3.1415926534 / 180;
+    double right_corner_width = tan(hFov / 2 * radian_conv);
+    double right_corner_height = tan(vFov / 2 * radian_conv);
+    double width = 2 * right_corner_width;
+    double height = 2 * right_corner_height;
+
+    double x_camera = x * width - right_corner_width;
+    double y_camera = y * height - right_corner_height;
+    double z_camera = -1;
+
+    Vector3D red = Vector3D(x_camera, y_camera, z_camera);
+    
+    //Uniformly sample the disk representing the thin lens at ....
+    //Calculate pFocus by intersecting the plane of focus with the red segment.
+    auto pLens = Vector3D((this->lensRadius) * sqrt(rndR) * cos(rndTheta), (this->lensRadius) * sqrt(rndR) * sin(rndTheta), 0);
+    auto pFocus =  red* this->focalDistance - pLens;
+
+    //Calculate the ray that originates from pLens, and set its direction towards pFocus (blue segment in the figure).
+    //Normalize the direction of the ray, perform the camera - to - world conversion for both its originand 
+    //direction, add pos to the ray's origin, and set the near and far clips to be the same as in Project 3-1, Part 1.
+    auto direction = this->c2w * pFocus;
+    direction.normalize();
+
+    auto ray = Ray(this->c2w * pLens + this->pos, direction, this->fClip);
+    ray.min_t = this->nClip;
+    return ray;
+
 }
 
 
